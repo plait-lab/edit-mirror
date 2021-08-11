c556febc8f93abe9cebc8475e1ef28c63ce31845
Justin Lubin <jlubi333@gmail.com>
2021-08-11 14:43:09 -0700

    Touch up init.sh

--- init.sh
+++ init.sh
@@ -20,9 +20,12 @@
 #   .gitignore
 #   last_upload_attempt.txt
 
+# TODO consent
+
+read -e -p "What is your Edit Mirror ID? " id
+read -e -p "How many years of experience do you have programming in statically-typed functional programming languages (such as Elm)? " exp
+
 em=___edit-mirror___
-id=0
-exp=5
 
 mkdir $em
 mkdir $em/logs
@@ -32,3 +35,5 @@ echo $id > $em/id.txt
 echo "Experience: $exp" > $em/demographics.txt
 echo "/*" > $em/.gitignore
 echo "0" > $em/last_upload_attempt.txt
+
+echo "Edit Mirror setup has completed."
