cf3e48b3711e9385d2b3b201e2922c4cb6b33500
Justin Lubin <jlubi333@gmail.com>
2022-01-11 18:11:10 -0800

    Progress toward showMessage

---
+++ Test.elm
@@ -0,0 +1,13 @@
+iiiiihiiiihiiiiihi am editing this now hi there hi ther
+h   :this is me typing
+hihiii am t
+i
+ia
+hi there i am now going to do this
+this 
+wow
+i am typing now wowie
+this is me typinihhiiig
+aaaaaaa
+
+are
